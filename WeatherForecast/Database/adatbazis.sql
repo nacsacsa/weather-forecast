ca768d83c870e87c8b7daa64591684c5a06a3eb4
HorogGabriella <gabriellahorog6@gmail.com>
2024-09-28 11:35:24 +0200

    Adatbázis hozz

---
+++ WeatherForecast/Database/adatbazis.sql
@@ -0,0 +1,8 @@
+create table weather_forecast(
+id int,
+username varchar(250) not null,
+password varchar (250) not null,
+constraint pk_id primary key(id));
+
+CREATE DATABASE my_database;
+USE my_database;weather_forecast
