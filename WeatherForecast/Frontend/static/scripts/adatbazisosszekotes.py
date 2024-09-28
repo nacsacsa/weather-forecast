051646c82bf899ba392a419d6786bafab60fb67a
HorogGabriella <gabriellahorog6@gmail.com>
2024-09-28 13:01:23 +0200

    login felület és adatbázis összekötés

---
+++ WeatherForecast/Frontend/static/scripts/adatbazisosszekotes.py
@@ -0,0 +1,38 @@
+from flask import Flask, render_template, request, redirect, url_for, flash
+from flask_mysqldb import MySQL
+
+app = Flask(__name__)
+app.secret_key = 's3cr3t'
+
+# MySQL konfiguráció
+app.config['MYSQL_HOST'] = '3306'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'weather'
+app.config['MYSQL_DB'] = 'adatbazis'
+
+mysql = MySQL(app)
+
+@app.route('/')
+def home():
+    return render_template('login.html')
+
+@app.route('/login', methods=['POST'])
+def login():
+    username = request.form['username']
+    password = request.form['password']
+
+    # Ellenőrizzük az adatbázisban
+    cur = mysql.connection.cursor()
+    cur.execute("SELECT * FROM weather_forecast WHERE username = %s AND password = %s", (username, password))
+    user = cur.fetchone()
+    cur.close()
+
+    if user:
+        flash('Sikeres bejelentkezés!')
+        return redirect(url_for('home'))  # Itt átirányíthatod a felhasználót egy másik oldalra
+    else:
+        flash('Hibás felhasználónév vagy jelszó.')
+        return redirect(url_for('home'))
+
+if __name__ == '__main__':
+    app.run(debug=True)
